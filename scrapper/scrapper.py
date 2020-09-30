ca620b4974790e8b4a7a5e38caf8d92fb34f9012
anuj-chourasiya <anujchourasiyadp@gmail.com>
2020-09-30 15:39:35 +0000

    scrap data from different job portals

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,3 +1,21 @@
-def main():
-    pass
-main()
+#!/usr/bin/env python
+from scrapper import linkedin
+from scrapper import monster
+from scrapper import indeed
+import server
+import fields
+import time
+
+#scrapping data for given job boards and other intenet sites
+while(True):
+    def main():
+        for location in fields.locations:
+            for job_title in fields.job_titles:
+
+                linkedin.scrape(location,job_title)
+                monster.scrape(location,job_title)
+                indeed.scrape(location,job_title)
+                server.saveData()
+                
+    main()
+    time.sleep(200000)
