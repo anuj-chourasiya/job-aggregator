3924c4a42ad3fe1102d575e76acad765cb813f41
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2020-10-07 11:59:44 +0530

    Update fields.py

--- scrapper/fields.py
+++ scrapper/fields.py
@@ -1,9 +1,7 @@
-# list of locations in India to scrap 
+# list of locations in India to scrap
 locations=['Bengaluru,Karnataka,India','Pune,Maharashtra,India']
-    
+
 # list of job titles to scrap
-job_titles=['Software Engineer']
-'''
-,'Software Development','Java Developer',
+job_titles=['Software Engineer','Software Development','Java Developer',
 'Python Developer','Django Developer','.NET Framework','Spring Framework'
-'''
+]
