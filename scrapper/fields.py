7c56dbd87b911ebac4e3615e2550ded44a354914
root <root@ip-172-31-8-52.ap-south-1.compute.internal>
2020-10-18 11:09:28 +0000

    added some more locations and job titles

--- scrapper/fields.py
+++ scrapper/fields.py
@@ -1,7 +1,7 @@
 # list of locations in India to scrap
-locations=['Bengaluru,Karnataka,India','Pune,Maharashtra,India']
+locations=['Bengaluru,Karnataka,India','Pune,Maharashtra,India','Hyderabad,Telangana,India','Indore,Madhya,Pradesh,India','Bhopal,Madhya,Pradesh,India','Mumbai,Maharashtra,India']
 
 # list of job titles to scrap
 job_titles=['Software Engineer','Software Development','Java Developer',
-'Python Developer','Django Developer','.NET Framework','Spring Framework'
-]
+'Python Developer','Django Developer','.NET Framework','Spring Framework']
+
