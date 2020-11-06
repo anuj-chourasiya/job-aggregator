70e0f776fbd4a6f5bfdd2671db1774d2b9ac8601
Anuj Chourasiya <anujchourasiyadp@gmail.com>
2020-11-06 12:07:35 +0530

    loggs added

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -5,17 +5,29 @@ from scrapper import indeed
 import server
 import fields
 import time
+import utils
 
+logger=utils.getParameters("scrapper.log")
 #scrapping data for given job boards and other intenet sites
 while(True):
     def main():
         for location in fields.locations:
             for job_title in fields.job_titles:
-                print("calling fields ",location,job_title)
-                linkedin.scrape(location,job_title)
-                monster.scrape(location,job_title)
-                indeed.scrape(location,job_title)
-                server.saveData()
+                logger.info("calling scrape for location {} and job title {}".format(location,job_title))
+                
+                linked_data=linkedin.scrape(location,job_title)
+                logger.info("linked in data: {}".format(linked_data))
+                monster_data=monster.scrape(location,job_title)
+                logger.info("monster data: {}".format(monster_data))
+                indeed_data=indeed.scrape(location,job_title)
+                logger.info("indeed data: {}".format(indeed_data))
+
+                all_data=[]
+                all_data.extend(linked_data)
+                all_data.extend(monster_data)
+                all_data.extend(indeed_data)
+
+                server.saveData(all_data)
                 
     main()
     time.sleep(21600)
